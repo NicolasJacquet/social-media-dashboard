33dd2e0ba2f88a58149d61e4f2d01004946b59bd
Nicolas Jacquet <nicolasjacquet89@gmail.com>
2020-05-01 15:59:40 +0200

    customize color-switcher

--- src/components/color-switcher.jsx
+++ src/components/color-switcher.jsx
@@ -1,15 +1,40 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import classnames from "classnames";
+import { useDispatch, useSelector } from "react-redux";
 import { switchThemeColor } from "./../redux";
+import css from "./../assets/styles/color-switcher.module.scss";
 
 const ColorSwitcher = () => {
     const dispatch = useDispatch();
+    const isLight = useSelector((state) => state);
 
     const onSwitch = () => {
         dispatch(switchThemeColor());
     };
 
-    return <button onClick={onSwitch}>Switch theme color</button>;
+    return (
+        <div className={css.switchContainer}>
+            <p className={css.text}>Dark mode</p>
+            <div>
+                <input
+                    id='switcher'
+                    type='checkbox'
+                    onChange={onSwitch}
+                    checked={isLight}
+                    className={css.input}
+                />
+                <label
+                    className={classnames(css.label, {
+                        [css.dark]: !isLight,
+                        [css.light]: isLight,
+                    })}
+                    htmlFor='switcher'
+                >
+                    Switch theme
+                </label>
+            </div>
+        </div>
+    );
 };
 
 export default ColorSwitcher;
